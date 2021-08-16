4f295028d0f6577ec27ffcd9c0af2d32148958c3
Gabriel Carvalho <gabrielc@backbase.com>
2021-08-16 13:36:18 -0300

    Correção no menu para responsividade

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, ElementRef, HostBinding, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostBinding, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MenuItem } from '../model';
 import { gsap } from 'gsap';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-menu',
@@ -9,33 +10,50 @@ import { gsap } from 'gsap';
 })
 export class MenuComponent implements AfterViewInit {
 
-  @HostBinding('class') classes = 'd-flex align-center';
+  @HostBinding('class.sm') classesSm = this.breakpointObserver.isMatched('(max-width: 1023px)');
+  @HostBinding('class.bg') classesBg = this.breakpointObserver.isMatched('(min-width: 1024px)');
+
+  showMenu = false;
 
   @ViewChild('items') items!: ElementRef<HTMLDivElement>;
 
   lastNum = -20;
 
   menus: MenuItem[] = [
-    { title: 'Início', section: '#' },
     { title: 'Sobre', section: '#sobre' },
     { title: 'Serviços', section: '#servicos' },
     { title: 'Nossa Equipe', section: '#equipe' },
     { title: 'Localização', section: '#localizacao' },
   ];
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver, private cdr: ChangeDetectorRef) { }
 
   ngAfterViewInit(): void {
-    gsap.from(this.items.nativeElement.children, { 
-      opacity: 0,
-      duration: .55,
-      y: () => {
-        this.lastNum *= -1;
-        return this.lastNum;
-      },
-      stagger: .5,
-      ease: 'ease.inout'
-    })
+    this.breakpointObserver.observe('(min-width: 1024px)')
+      .subscribe((data: any) => {
+        if (data.matches) {
+          gsap.from(this.items.nativeElement.children, {
+            opacity: 0,
+            duration: .55,
+            y: () => {
+              this.lastNum *= -1;
+              return this.lastNum;
+            },
+            stagger: .5,
+            ease: 'ease.inout'
+          });
+        }
+      });
+  }
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.classesSm = this.breakpointObserver.isMatched('(max-width: 1023px)');
+    this.classesBg = this.breakpointObserver.isMatched('(min-width: 1024px)');
   }
 
 }
